cf8b31b182f2b551b9c61612b0b8eebae620660e
mallar-B <137499848+mallar-B@users.noreply.github.com>
2024-05-07 23:54:24 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 installed=$(which yay)
 if [[ $installed != "/usr/bin/yay" ]]; then 
 	sudo pacman -S --needed git
-	git clone https://aur.archlinux.com/yay
+	git clone https://aur.archlinux.org/yay
 	wait
 	cd yay 
 	makepkg -si
