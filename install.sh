1471e1adf9303a2ef85d658030408c46ccc6ede1
mallar-B <137499848+mallar-B@users.noreply.github.com>
2024-05-08 00:42:09 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+/#!/bin/bash
 
 # check if yay is installed
 
@@ -12,7 +12,7 @@ if [[ $installed != "/usr/bin/yay" ]]; then
 	wait
 fi
 
-userConfDir= "/home/"$user"/.config/"
+userConfDir= "/home/"$(whoami)"/.config/"
 while IFS= read -r package #IFS is special variable to fine whitespaces
 do
 	#Skip emty lines and comments in package.txt
