f7c04cf8abec07559d53979d066038e5a2c1d1f7
mallar-B <137499848+mallar-B@users.noreply.github.com>
2024-05-08 00:23:37 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,14 @@ if [[ $installed != "/usr/bin/yay" ]]; then
 	makepkg -si
 	wait
 fi
+
+user=$(whoami)
+while IFS= read -r package #IFS is special variable to fine whitespaces
+do
+	#Skip emty lines and comments in package.txt
+	if [[ -z "$package" || "$package" == "#"* ]]; then
+		continue
+	fi
+	
+	yay -S --needed --noconfirm "$package"
+done < "./packages/dependencies.txt"
