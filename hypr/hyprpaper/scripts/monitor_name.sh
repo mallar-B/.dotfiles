ea6023d954b4541a39c0c82c1c484709331e2873
mallar-B <137499848+mallar-B@users.noreply.github.com>
2024-05-08 14:35:01 +0000

    Update monitor_name.sh

--- hypr/hyprpaper/scripts/monitor_name.sh
+++ hypr/hyprpaper/scripts/monitor_name.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-
 # Run xrandr command to get information about monitor outputs
+
 xrandr_output=$(xrandr)
 
-# Use grep to filter out lines containing information about monitor outputs
-monitor_lines=$(echo "$xrandr_output" | grep -E '\b[A-Z]+-[A-Z]+-[0-9]+')
-
-# Loop through each line to extract the monitor output names
 while IFS= read -r line; do
-    # Extract the monitor output name using awk or any other appropriate method
-    monitor_name=$(echo "$line" | awk '{print $1}')
-    echo "$monitor_name"
-done <<< "$monitor_lines"
-
+    # Check if the line contains " connected"
+    if [[ $line == *" connected"* ]]; then
+        # Split the line by space and get the first element
+        monitor_name=$(echo "$line" | awk '{print $1}')
+    fi
+done <<< "$xrandr_output"
+echo "$monitor_name"
 
