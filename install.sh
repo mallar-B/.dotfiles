b7f587d20915116dfa10970cd24c928cdba70653
mallar-B <137499848+mallar-B@users.noreply.github.com>
2024-05-08 11:49:34 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,6 +34,21 @@ cp -r config.js style.css widgets $userConfDir/ags/
 gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3-dark"
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark' # for gtk4 apps
 echo "Theme is set to adw-gtk3-dark"
+sleep 2
+
+# Add Wallpaper for hyprpaper
+# URL of the API endpoint to fetch random wallpapers from Wallhaven.cc
+API_URL="https://wallhaven.cc/api/v1/search?sorting=random"
+
+# Download the JSON response containing information about random wallpapers
+response=$(curl -s "$API_URL")
+
+# Parse the JSON response to extract the URL of the random wallpaper
+wallpaper_url=$(echo "$response" | jq -r '.data[0].path')
+
+# Download the wallpaper using wget
+wget "$wallpaper_url" -P ~/Downloads/
+echo "Random wallpaper downloaded to ~/Pictures/Wallpapers"
 
 # my personal packages install
 if [[ $(whoami) == "mallarb" ]]; then
