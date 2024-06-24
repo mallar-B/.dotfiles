b1876d135b53be3c25cca110c048259ec972c479
mallar-B <137499848+mallar-B@users.noreply.github.com>
2024-06-24 12:19:00 +0000

    audio options enhancement

--- .config/ags/widgets/audio_options/mic.js
+++ .config/ags/widgets/audio_options/mic.js
@@ -1,34 +1,46 @@
-const audio = await Service.import("audio")
-const hyprland = await Service.import("hyprland")
+const audio = await Service.import("audio");
+const hyprland = await Service.import("hyprland");
+
+export const togglePavucontrol = () => {
+  console.log("pressed")
+  if (Utils.exec(`pidof pavucontrol`)) {
+    Utils.execAsync(["pkill", "pavucontrol"]).catch((err) => console.log(err));
+  } else {
+    hyprland.messageAsync(`dispatch exec [ float; size 50% ] pavucontrol`);
+  }
+};
 
 const icon = Widget.Icon({
-    class_name: "audio-icon",
-}).hook(audio.microphone, self =>{
-    const icons = {
-        on: "audio-input-microphone-symbolic",
-        off: "microphone-disabled-symbolic",
-    };
-    self.icon = audio.microphone.is_muted ? `${icons.off}` : `${icons.on}`;
-})
+  class_name: "audio-icon",
+}).hook(audio.microphone, (self) => {
+  const icons = {
+    on: "audio-input-microphone-symbolic",
+    off: "microphone-disabled-symbolic",
+  };
+  self.icon = audio.microphone.is_muted ? `${icons.off}` : `${icons.on}`;
+});
 
-const micLabel = Widget.Label().hook(audio.microphone,self => {
-    self.label = " " + Math.round(audio.microphone.volume * 100).toString() + "%";
-})
+const micLabel = Widget.Label().hook(audio.microphone, (self) => {
+  self.label = " " + Math.round(audio.microphone.volume * 100).toString() + "%";
+});
 
 const micIndicator = Widget.Box({
-    css:"margin-left: 5px",
-    class_name: "audiobox",
-    children: [icon, micLabel],
-}).hook(audio.speaker, self =>{
-    self.tooltip_text = audio.microphone.name;
-})
-
+  css: "margin-left: 5px",
+  class_name: "audiobox",
+  children: [icon, micLabel],
+}).hook(audio.speaker, (self) => {
+  self.tooltip_text = audio.microphone.name;
+});
 
 const Mic = Widget.EventBox({
-    child: micIndicator,
-    onScrollUp: () =>{audio.microphone.volume += 0.03},
-    onScrollDown: () => {audio.microphone.volume -= 0.03},
-    onPrimaryClick: () => { hyprland.messageAsync(`dispatch exec [ float; size 50% ] pavucontrol`) },
-    onSecondaryClick: () => { Utils.execAsync(['pkill', 'pavucontrol']).catch(err => console.log(err)) }
-})
-export default Mic
+  child: micIndicator,
+  onScrollUp: () => {
+    audio.microphone.volume += 0.03;
+  },
+  onScrollDown: () => {
+    audio.microphone.volume -= 0.03;
+  },
+  onPrimaryClick: togglePavucontrol,
+  onSecondaryClick: () => { },
+});
+export default Mic;
--- .config/ags/widgets/audio_options/speaker.js
+++ .config/ags/widgets/audio_options/speaker.js
@@ -1,58 +1,80 @@
-const audio = await Service.import("audio")
-const hyprland = await Service.import("hyprland")
+import { togglePavucontrol } from "./mic.js";
+
+const audio = await Service.import("audio");
+const changeAudioOutput = () => {
+  // store only the sink names in sources
+  let sources = Utils.exec(`bash -c "pactl list short sinks | cut -f2"`)
+    .trim()
+    .split(/\s+/);
+  // get the current sink name
+  let currentSource = Utils.exec(`pactl get-default-sink`);
+  // change sink to next available sink
+  for (let i = 0; i < sources.length; i++) {
+    if (currentSource == sources[i] && i == sources.length - 1) {
+      Utils.exec(`pactl set-default-sink ${sources[0]}`);
+    } else {
+      Utils.exec(`pactl set-default-sink ${sources[i + 1]}`);
+    }
+  }
+};
 
 const icons = {
-    101: "overamplified",
-    67: "high",
-    34: "medium",
-    1: "low",
-    0: "muted",
-}
+  101: "overamplified",
+  67: "high",
+  34: "medium",
+  1: "low",
+  0: "muted",
+};
 
 function getIcon() {
-    const icon = audio.speaker.is_muted ? 0 : [101, 67, 34, 1, 0].find(
-        threshold => threshold <= audio.speaker.volume * 100)
+  const icon = audio.speaker.is_muted
+    ? 0
+    : [101, 67, 34, 1, 0].find(
+      (threshold) => threshold <= audio.speaker.volume * 100,
+    );
 
-    return `audio-volume-${icons[icon]}-symbolic`
+  return `audio-volume-${icons[icon]}-symbolic`;
 }
 
 export const icon = Widget.Icon({
-    class_name: "audio-icon",
-    icon: Utils.watch(getIcon(), audio.speaker, getIcon),
-})
+  class_name: "audio-icon",
+  icon: Utils.watch(getIcon(), audio.speaker, getIcon),
+});
 
-export const volumeLabel = Widget.Label().hook(audio.speaker, self =>{
-    self.label = " " + Math.round(audio.speaker.volume * 100).toString() + "%";
-})
+export const volumeLabel = Widget.Label().hook(audio.speaker, (self) => {
+  self.label = " " + Math.round(audio.speaker.volume * 100).toString() + "%";
+});
 
 const volumeIndicator = Widget.Box({
-    class_name: "audiobox",
-    children: [icon, volumeLabel,]
-}).hook(audio.speaker, self =>{
-    self.tooltip_text = audio.speaker.name;
-})
+  class_name: "audiobox",
+  children: [icon, volumeLabel],
+}).hook(audio.speaker, (self) => {
+  self.tooltip_text = audio.speaker.name;
+});
 
-const volumeUp = () =>{
+const volumeUp = () => {
+  if (!audio.speaker) return;
+  if (audio.speaker.volume <= 0.09) audio.speaker.volume += 0.01;
+  else audio.speaker.volume += 0.03;
+};
+
+const Speaker = Widget.EventBox({
+  child: volumeIndicator,
+  onScrollUp: () => {
     if (!audio.speaker) return;
     if (audio.speaker.volume <= 0.09) audio.speaker.volume += 0.01;
     else audio.speaker.volume += 0.03;
-}
-
-const Speaker = Widget.EventBox({
-    child: volumeIndicator,
-    onScrollUp: () =>{
-        if (!audio.speaker) return;
-        if (audio.speaker.volume <= 0.09) audio.speaker.volume += 0.01;
-        else audio.speaker.volume += 0.03;
-    },
-    onScrollDown: () =>{
-        if (!audio.speaker) return;
-        if (audio.speaker.volume <= 0.09) audio.speaker.volume -= 0.01;
-        else audio.speaker.volume -= 0.03;
-    },
-    onMiddleClick: () =>{ audio.speaker.is_muted = !audio.speaker.is_muted },
-    onPrimaryClick: () => { hyprland.messageAsync(`dispatch exec [ float; size 50%; ] pavucontrol`) },
-    onSecondaryClick: () => { Utils.execAsync(['pkill', 'pavucontrol']).catch(err => console.log(err)) }
-})
+  },
+  onScrollDown: () => {
+    if (!audio.speaker) return;
+    if (audio.speaker.volume <= 0.09) audio.speaker.volume -= 0.01;
+    else audio.speaker.volume -= 0.03;
+  },
+  onMiddleClick: () => {
+    audio.speaker.is_muted = !audio.speaker.is_muted;
+  },
+  onPrimaryClick: togglePavucontrol,
+  onSecondaryClick: changeAudioOutput,
+});
 
 export default Speaker;
