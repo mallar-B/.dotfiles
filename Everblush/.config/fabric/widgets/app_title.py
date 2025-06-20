630869977aef209618874a8261c6fcb23f3d3694
mallar-B <bhattacharyamallar@gmail.com>
2025-06-20 21:58:46 +0530

    different app title on fullscreen, took 1 year of my lifespan

--- Everblush/.config/fabric/widgets/app_title.py
+++ Everblush/.config/fabric/widgets/app_title.py
@@ -1,4 +1,7 @@
+import json
 from fabric.hyprland.widgets import ActiveWindow
+from fabric.hyprland.widgets import get_hyprland_connection
+from fabric.utils import bulk_connect
 from fabric.utils import FormattedString, truncate
 
 
@@ -8,7 +11,49 @@ class AppTitle(ActiveWindow):
             name="app-title",
             formatter=FormattedString(
                 "{'' if not win_title or win_title == 'unknown' else truncate(win_title, 42)}",
-                truncate=truncate
+                truncate=truncate,
             ),
             **kwargs
         )
+        self.curr_full_screens = set()
+
+        bulk_connect(
+            # self.connection comes from ActiveWindow
+            self.connection,
+            {
+                "event::fullscreen": self.set_full_screen,
+                "event::closewindow": self.unset_full_screen,
+                "event::activewindow": self.check_full_screen,
+            },
+        )
+
+    def set_full_screen(self, *_):
+        # This will return the result of  hyprctl activewindow as a string
+        activeWindowStr = self.connection.send_command("j/activewindow").reply.decode()
+        # Read as json
+        # Possible value 0,1,2
+        fullScreenValue = json.loads(activeWindowStr).get("fullscreen")
+        windowId = json.loads(activeWindowStr).get("address").lstrip("0x")
+        if fullScreenValue != 0:
+            self.curr_full_screens.add(windowId)
+            print("current ids are", self.curr_full_screens)
+            self.check_full_screen()
+            return
+        self.curr_full_screens.remove(windowId)
+        self.check_full_screen()
+        print("current ids are", self.curr_full_screens)
+
+    def unset_full_screen(self, _, event):
+        print("called by event")
+        print(type(self.curr_full_screens))
+        print("removing", event.data)
+        self.curr_full_screens.remove(str(event.data[0]))
+        self.check_full_screen()
+
+    def check_full_screen(self, *_):
+        activeWindowStr = self.connection.send_command("j/activewindow").reply.decode()
+        windowId = json.loads(activeWindowStr).get("address").lstrip("0x")
+        if windowId in self.curr_full_screens:
+            self.set_name("app-title-fullscreen")
+        else:
+            self.set_name("app-title")
