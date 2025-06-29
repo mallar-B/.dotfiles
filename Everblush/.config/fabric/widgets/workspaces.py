77ad1454ece6481ace40969915f63c67856f3965
mallar-B <bhattacharyamallar@gmail.com>
2025-06-29 11:50:28 +0530

    do not show special workspaces

--- Everblush/.config/fabric/widgets/workspaces.py
+++ Everblush/.config/fabric/widgets/workspaces.py
@@ -4,10 +4,12 @@ from fabric.hyprland.widgets import (
 )
 from utils import add_cursor_hover
 
+
 class WorkspaceButton(HyprlandWorkspaceButton):
     def __init__(self, **kwargs):
         super().__init__(name="workspace-button", **kwargs)
         add_cursor_hover(self, "pointer")
+
 
 class Workspaces(HyprlandWorkSpaces):
     def __init__(self, **kwargs):
@@ -15,7 +17,10 @@ class Workspaces(HyprlandWorkSpaces):
             name="workspaces-container",
             buttons=None,
             buttons_factory=lambda workspace_id: WorkspaceButton(
-                id=workspace_id, label=str(workspace_id)
+                # Do not show -98 for special workspace
+                id=workspace_id,
+                label=str(workspace_id),
+                visible=True if workspace_id > 0 else False,
             ),
             **kwargs
         )
